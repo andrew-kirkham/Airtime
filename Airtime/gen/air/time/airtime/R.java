3e49b6056fe7f0153d2006ced63f6e1a8fb48d10
andrew-kirkham <ardentcommando@gmail.com>
2014-03-26 20:39:33 -0500

    test commit adding a new activity

--- Airtime/gen/air/time/airtime/R.java
+++ Airtime/gen/air/time/airtime/R.java
@@ -29,16 +29,20 @@ public final class R {
         public static final int container=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_home=0x7f030000;
-        public static final int fragment_home=0x7f030001;
+        public static final int activity_details=0x7f030000;
+        public static final int activity_home=0x7f030001;
+        public static final int fragment_details=0x7f030002;
+        public static final int fragment_home=0x7f030003;
     }
     public static final class menu {
-        public static final int home=0x7f070000;
+        public static final int details=0x7f070000;
+        public static final int home=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_details=0x7f050003;
     }
     public static final class style {
         /** 
