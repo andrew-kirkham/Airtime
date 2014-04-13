d7468773c952080f9e1b92449cf9555c95d09a6b
Jenny Reiman <jennifer.reiman@marquette.edu>
2014-04-13 17:50:06 -0500

    Detailed Favorites layout made

--- Airtime/src/com/airtime/DetailedFavorite.java
+++ Airtime/src/com/airtime/DetailedFavorite.java
@@ -11,6 +11,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.os.Build;
 
 /**
@@ -21,54 +23,28 @@ import android.os.Build;
 
 public class DetailedFavorite extends Activity {
 
-	private Show favorite;
+	Show favorite = new Show();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detailed_favorite);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
+		
+		TextView tvShowName = (TextView) findViewById(R.id.TVshowTitle);
+		tvShowName.setText(favorite.Name);
+		
+		TextView tvShowNetwork = (TextView) findViewById(R.id.TVshowNetwork);
+		tvShowNetwork.setText(favorite.Network);
+		
+		TextView lastAired = (TextView) findViewById(R.id.LastAriedText);
+		//tvShowNetwork.setText(favorite.LastEpisode);;
+		
+		TextView nextEp = (TextView) findViewById(R.id.NextEpText);
+		//tvShowNetwork.setText(favorite.NextEpisode);
+		
+		TextView status = (TextView) findViewById(R.id.StatusText);
+		//tvShowNetwork.setText(favorite.Status);
+		
+		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.detailed_favorite, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(
-					R.layout.fragment_detailed_favorite, container, false);
-			return rootView;
-		}
-	}
-
 }
